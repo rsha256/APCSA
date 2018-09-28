dfd6a6b36c35a2d3f28cf2a5c80b21d67109056f
Rahul Shah <20RS0586@wwprsd.org>
2018-09-28 10:49:37 -0400

    add some math stuff

--- Math/src/MathFuncts.java
+++ Math/src/MathFuncts.java
@@ -1,13 +1,36 @@
 public class MathFuncts {
 
+	/*
+	 * static 
+	 */
 	public static double average(int i, int j, int k) {
 		return (double) (i + j + k) / 3;
 	}
 
+	public static int makeChange(double cents) {
+		return (int)(cents / .25);
+	}
+	
+	public static double roundToHths(double d) {
+		double t100 = d * 100.0;
+		int t1 = (int)(t100 + 0.5);
+		return (t1/100.0);
+	}
+	
+	public static int roundToHds(double d) {
+		double t100 = d / 100.0;
+		int t1 = (int)(t100 + .5);
+		return (int)(t1 * 100.0);
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("The average of 3, 4, and 12 is: " + average(3, 4, 12));
-		System.out.println("10 cel is: " + convertTemp(10));
-
+		System.out.println("The average of 3, 4, and 12 is: " 
+				+ average(3, 4, 12));
+		System.out.println("10 cel is: " 
+				+ convertTemp(10));
+		System.out.println("Return: " + makeChange(.78) + " quarters");
+		System.out.println(roundToHths(9.236));
+		System.out.println(roundToHds(1297));
 	}
 
 	// this method converts an inputted Celsius temperature into F
