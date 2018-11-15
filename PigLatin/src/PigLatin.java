08227563eddfa10ccfc52451b2a4274d948c5c8e
Rahul Shah <rahuldshah3@gmail.com>
2018-11-15 09:45:48 -0500

    add pl fin

--- PigLatin/src/PigLatin.java
+++ PigLatin/src/PigLatin.java
@@ -1,81 +1,78 @@
 public class PigLatin {
 	public static void main(String[] args) {
-		System.out.println(translate("test says rahul"));
+		System.out.println(translate("Ecksdee"));
 	}
+
 	public static String translate(String englishWord) {
 		String temp = englishWord;
 		String[] englishWordBySpaces = temp.split(" ");
 		String[] pigLatinWord = new String[englishWord.length()];
-		
+
 		int counter = 0;
-		
+
 		for (String s : englishWordBySpaces) {
 			pigLatinWord[counter] = lTranslate(s);
 			counter++;
 		}
 		String returnString = "";
-		for(String x : pigLatinWord) {
+		for (String x : pigLatinWord) {
 			if (x != null)
-			  returnString = returnString + " " + x; 
+				returnString = returnString + " " + x;
 		}
-		return returnString.substring(1); 
+		return returnString.substring(1);
 	}
-	
+
 	public static int min(int a, int b) {
-		if(a == -1) {
+		if (a == -1) {
 			a = Integer.MAX_VALUE;
 		}
-		if(b == -1) {
+		if (b == -1) {
 			b = Integer.MAX_VALUE;
 		}
 		return Math.min(a, b);
 	}
-	
+
 	public static String lTranslate(String englishWord) {
+		String xString = englishWord;
 		englishWord = englishWord.toLowerCase();
-		if (!(englishWord.contains("a") ||
-				englishWord.contains("e") || 
-				englishWord.contains("i") || 
-				englishWord.contains("o") ||
-				englishWord.contains("u"))) {
-			return (englishWord.charAt(0)+"").toUpperCase() + englishWord.substring(1) + "ay";
-		}
-		else if(englishWord.charAt(0) == 'a' ||
-				englishWord.charAt(0) == 'e' || 
-				englishWord.charAt(0) == 'i' || 
-				englishWord.charAt(0) == 'o' ||
-				englishWord.charAt(0) == 'u') {
-			return (englishWord.charAt(0)+"").toUpperCase() + englishWord.substring(1) + "way";
-		}
-		else if((englishWord.contains("a") ||
-				englishWord.contains("e") || 
-				englishWord.contains("i") || 
-				englishWord.contains("o") ||
-				englishWord.contains("u")) && !(englishWord.charAt(0) == 'a' ||
-				englishWord.charAt(0) == 'e' || 
-				englishWord.charAt(0) == 'i' || 
-				englishWord.charAt(0) == 'o' ||
-				englishWord.charAt(0) == 'u')) {
+		if (!(englishWord.contains("a") || englishWord.contains("e") || englishWord.contains("i")
+				|| englishWord.contains("o") || englishWord.contains("u"))) {
+			if (Character.isUpperCase(xString.charAt(0))) {
+				return (englishWord.charAt(0) + "").toUpperCase() + englishWord.substring(1) + "ay";
+			} else {
+				return englishWord + "ay";
+			}
+		} else if (englishWord.charAt(0) == 'a' || englishWord.charAt(0) == 'e' || englishWord.charAt(0) == 'i'
+				|| englishWord.charAt(0) == 'o' || englishWord.charAt(0) == 'u') {
+			if (Character.isUpperCase(xString.charAt(0))) {
+
+				return (englishWord.charAt(0) + "").toUpperCase() + englishWord.substring(1) + "way";
+			} else {
+				return englishWord + "way";
+			}
+		} else if ((englishWord.contains("a") || englishWord.contains("e") || englishWord.contains("i")
+				|| englishWord.contains("o") || englishWord.contains("u"))
+				&& !(englishWord.charAt(0) == 'a' || englishWord.charAt(0) == 'e' || englishWord.charAt(0) == 'i'
+						|| englishWord.charAt(0) == 'o' || englishWord.charAt(0) == 'u')) {
 			int aFirstVowel = englishWord.indexOf('a');
 			int eFirstVowel = englishWord.indexOf('e');
 			int iFirstVowel = englishWord.indexOf('i');
 			int oFirstVowel = englishWord.indexOf('o');
 			int uFirstVowel = englishWord.indexOf('u');
-			
+
 			int firstVowel = min(min(min(min(aFirstVowel, eFirstVowel), iFirstVowel), oFirstVowel), uFirstVowel);
 			String start = englishWord.substring(0, firstVowel);
 			String end = englishWord.substring(firstVowel);
-			
-			if(Character.isUpperCase(englishWord.charAt(0))) {
+
+			if (Character.isUpperCase(xString.charAt(0))) {
 				start = start.toLowerCase();
-				end = (end.charAt(0)+"").toUpperCase() + englishWord.substring(1);
-			} 
-			
+				end = (end.charAt(0) + "").toUpperCase() + end.substring(1);
+			}
+
 			return end + start + "ay";
-		}
-		else {
+		} else {
 			return englishWord + " couldn't be translated!";
 		}
 	}
-	
+
 }
