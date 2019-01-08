96a02e4d53c3c88006fb6b9823917ee3ba59b2df
Rahul Shah <rahuldshah3@gmail.com>
2019-01-08 10:44:35 -0500

    add broken tally

--- Arrays/src/ArrayExample.java
+++ Arrays/src/ArrayExample.java
@@ -43,7 +43,7 @@ public class ArrayExample {
 	// 5. This method calculates and returns the average of all of the elements
 	// The average of the default array is 14.1
 	public double average() {
-		int tot = 0;
+		double tot = 0;
 		for (int i : numbArray) {
 			tot += i;
 		}
@@ -77,20 +77,47 @@ public class ArrayExample {
 	 * default constructor will be Number Frequency 1 1 3 2 7 2 15 1 19 3 48 1
 	 */
 	public void tallyList() {
-
-		int unique = 0;
-		for (int i = 0; i < numbArray.length; i++) {
-			for (int j = i + 1; j < numbArray.length; j++)
-				if (numbArray[i] != numbArray[j])
-					unique++;
-
-		}
-		int[] tally = new int[unique];
-
+		Arrays.sort(numbArray);
+		int num = numbArray[0];
 		int count = 0;
-		for (int x : numbArray) {
-
+		for (int i = 1; i < numbArray.length; i++) {
+			if (num == numbArray[i])
+				count++;
+			else {
+				count++;
+				System.out.println(num + " " + count);
+				num = numbArray[i];
+				count = 0;
+			}
 		}
+		count++;
+		System.out.println(num + " " + count);
+
+		// int unique = 0;
+		// for (int i = 0; i < numbArray.length; i++) {
+		// for (int j = i + 1; j < numbArray.length; j++)
+		// if (numbArray[i] != numbArray[j])
+		// unique++;
+		// }
+		// System.out.println(unique);
+		// int[] tally = new int[unique];
+		//
+		// for (int k = 0; k < unique; k++) {
+		// for (int j = 0; j < numbArray.length; j++) {
+		// for (int i = 0; i <= findMax(); i++)
+		// if (numbArray[j] == i)
+		// tally[k] = numbArray[j];
+		// }
+		// }
+		// int count = 0;
+		// for (int k = 0; k < tally.length; k++) {
+		// count = 0;
+		// for (int i = 0; i < numbArray.length; i++) {
+		// if (numbArray[i] == tally[k])
+		// count++;
+		// }
+		// System.out.println(numbArray[k] + " " + count);
+		// }
 	}
 
 	/**
@@ -100,6 +127,19 @@ public class ArrayExample {
 	 * 11.0 15 7 11.0 19 7 13.0 19 3 11.0 19 3 11.0 48 1 24.5
 	 */
 	public void listReverseAvg() {
+		int[] reverse = new int[numbArray.length];
+		for (int i = numbArray.length - 1; i >= 0; i--) {
+			reverse[numbArray.length - 1 - i] = numbArray[i];
+		}
+
+		double[] average = new double[numbArray.length];
+		for (int i = 0; i < numbArray.length; i++) {
+			average[i] = (numbArray[i] + reverse[i]) / 2.0;
+		}
+
+		for (int i = 0; i < numbArray.length; i++) {
+			System.out.println(numbArray[i] + " " + reverse[i] + " " + average[i]);
+		}
 
 	}
 
@@ -107,6 +147,7 @@ public class ArrayExample {
 	//
 	public void sort() {
 		Arrays.sort(numbArray);
+		display();
 	}
 
 	// 11. Binary Search.
@@ -115,28 +156,30 @@ public class ArrayExample {
 	// search.
 	// ex. Using the default array findIndex(15) will return 4
 	public int binarySearch(int lookFor) {
-		// return Arrays.binarySearch(numbArray, lookFor);
-		sort();
-		int middle = numbArray.length / 2;
-		int bottom = numbArray[0];
-		int top = numbArray[numbArray.length - 1];
-		while (bottom <= top) {
-			if (lookFor == numbArray[middle])
-				return middle;
-			else {
-				if (lookFor < middle)
-					top = middle - 1;
-				else
-					bottom = middle + 1;
-			}
-		}
-		return -1;
+		return Arrays.binarySearch(numbArray, lookFor);
+		// Arrays.sort(numbArray);
+		// int middle = numbArray.length / 2;
+		// int bottom = numbArray[0];
+		// int top = numbArray[numbArray.length - 1];
+		// while (bottom <= top) {
+		// if (lookFor == numbArray[middle])
+		// return middle - 1;
+		// else {
+		// if (lookFor < middle) {
+		// top = middle - 1;
+		// } else {
+		// bottom = middle + 1;
+		// }
+		// }
+		// }
+		// return -1;
 	}
 	// 12. Write a tester that will create a random array of size 50
 	// and test all of these methods
 
 	public static void main(String[] args) {
-
+		ArrayExample obj = new ArrayExample();
+		obj.tallyList();
 	}
 
 }
