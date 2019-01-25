5e2c5d9f9b0e5e66e41b8e728a44f8b7598ced54
Rahul Shah <rahuldshah3@gmail.com>
2019-01-25 10:52:12 -0500

    update hotel

--- Arrays/src/guidance/Hotel.java
+++ Arrays/src/guidance/Hotel.java
@@ -1,5 +1,72 @@
 package guidance;
 
 public class Hotel {
+	// list[] stores reservations for each room. The index is the room number.
+	// All elements are null at the beginning
+	private Reservation[] list;
 
+	public Hotel(int numbRooms) {
+		list = new Reservation[numbRooms];
+	}
+
+	// Displays room numbers and reservation information.
+	public void display() {
+		for (Reservation x: list) {
+			x.toString();
+		}
+			
+	}
+
+	// returns true if room is occupied
+	public boolean isOccupied(int room) {
+		return list[room].getName() == null;
+	}
+
+	// Assigns a new Reservation to the first available room.
+	// Print out reservation with room number or indicate that hotel is full
+	public void reserveRoom(String name, double rate) {
+		int count = 0;
+		for (Reservation x: list) {
+			if (x.getName() == null) {
+				list[count] = new Reservation(name, rate);
+				System.out.println("Room " + count + " is reserved by " + x.getName());
+				return;
+			}
+		count++;
+			if (count == list.length) {
+				System.out.println("Hotel is occupied");
+				return;
+			}
+		}
+	}
+
+	// Assigns a new Reservation to an inputted room.
+	// if room is occupied, find the first available room in the array
+	// Print out reservation with room number or indicate that hotel is full
+	public void reserveRoom(String name, double rate, int room) {
+		if (list[room].getName() == null)
+			list[room] = new Reservation(name, rate);
+		else
+			reserveRoom(name, rate);
+	}
+
+	// Find and return number of rooms with a reservation
+	public int reservedRoomCount() {
+		int count = 0;
+		for (Reservation reservation : list) {
+			if (reservation.getName() != null) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	// find and return the average room rate of the occupied rooms.
+	public double averageRoomRate() {
+		double aRate = 0;
+		for (Reservation reservation : list) {
+			aRate += reservation.getRate();
+		}
+		return aRate;
+	}
 }
