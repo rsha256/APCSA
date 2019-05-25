bc505c74a2c90275fe1c9fbe5188a4eca636278a
Rahul Shah <rahuldshah3@gmail.com>
2019-05-24 22:14:45 -0400

    almost over

--- APExam/src/Tester.java
+++ APExam/src/Tester.java
@@ -1,5 +1,27 @@
+import java.util.*;
+
 public class Tester {
-	public static void main(String[] args) {
-		System.out.println("yee");
-	}
+
+    public static boolean mystery(String str) {
+        String temp = "";
+
+        for (int k = str.length(); k>0; k--) {
+            temp = temp + str.substring(k-1, k);
+        }
+
+        System.out.println("temp: " + temp);
+        System.out.println("str: " + str);
+
+        return temp.equals(str);
+    }
+
+    public static void main(String[] args) {
+//        System.out.print(mystery("no"));
+        String s1 = "My name is GFG";
+
+        List<Integer> l = new List<Integer>();
+
+        // prints true
+        System.out.println(s1.contains("GFG"));
+    }
 }
